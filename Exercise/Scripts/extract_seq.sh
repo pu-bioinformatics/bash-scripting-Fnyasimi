82fb3765689602ee79273d1345a030cc9e5450a0
User <user@BioInfo3>
2019-02-26 09:55:19 +0300

    updated exercise code

--- Exercise/Scripts/extract_seq.sh
+++ Exercise/Scripts/extract_seq.sh
@@ -1,1 +1,1 @@
-less nrf1_seq.fa | grep ^'>' >> sequence_names.txt
+less /home/user/github/bash-scripting-Fnyasimi/Exercise/Data/nrf1_seq.fa | grep ^'>' >> /home/user/github/bash-scripting-Fnyasimi/Exercise/Results/sequence_names.txt
