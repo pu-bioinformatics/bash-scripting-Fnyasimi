46dee3a38358c7000ba7457c86c7327f419f8bf1
User <user@BioInfo3>
2019-03-01 08:44:51 +0300

    the script was updated to extract sequence names

--- Exercise/Scripts/extract_seq.sh
+++ Exercise/Scripts/extract_seq.sh
@@ -1,1 +1,4 @@
-less /home/user/github/bash-scripting-Fnyasimi/Exercise/Data/nrf1_seq.fa | grep ^'>' >> /home/user/github/bash-scripting-Fnyasimi/Exercise/Results/sequence_names.txt
+#!/bin/bash
+
+less /home/user/github/bash-scripting-Fnyasimi/Exercise/Data/nrf1_seq.fa | grep ^'>' > /home/user/github/bash-scripting-Fnyasimi/Exercise/Results/sequence_names.txt
+cut -d ' ' -f 1 /home/user/github/bash-scripting-Fnyasimi/Exercise/Results/sequence_names.txt | cut -c 2- > /home/user/github/bash-scripting-Fnyasimi/Exercise/Results/sequence_ids.txt
